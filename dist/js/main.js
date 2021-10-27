89bf1d804449520fedfbbf369884ac83eb5b6987
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2021-10-27 12:13:04 -0500

    mobile js and css done

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+if (window.screen.width <= 450) {
+  button1.addEventListener("click", (e) => {
+    let secondFooter = e.currentTarget.parentElement.nextElementSibling;
+    e.currentTarget.parentElement.classList.toggle("hide");
+    secondFooter.classList.toggle("hide");
+  });
+
+  button2.addEventListener("click", (e) => {
+    let firstFooter =
+      e.currentTarget.parentElement.parentElement.previousElementSibling;
+    firstFooter.classList.toggle("hide");
+    e.currentTarget.parentElement.parentElement.classList.toggle("hide");
+  });
+}
